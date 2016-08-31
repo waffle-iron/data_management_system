37b0e6fc63a34a703cb69c8248eeba92cb5e679b
Robert Olendorf <drolendorf@gmail.com>
2016-08-31 23:13:18 +0000

    chuck norris kicked jsonb's ass closes #4

--- spec/models/chuck_norris_spec.rb
+++ spec/models/chuck_norris_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ChuckNorris, type: :model do
-  it { should respond_to :fact }
-  it { should respond_to :knockouts }
+  it { should respond_to :kungfu }
+
+  let(:charles) { build :chuck_norris }
+
+  describe 'accessing the data with indifferent access' do
+    it 'should index with a string as the key' do
+      expect(charles.kungfu['fact']).to be_a_kind_of String
+    end
+    it 'should index with a symbol as the key' do
+      expect(charles.kungfu[:fact]).to be_a_kind_of String
+    end
+  end
 end
 
